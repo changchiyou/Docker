cb42ba5b9ddd49d40ca2fed9adbaf9988e004d87
Chris <changchiyou@gmail.com>
2025-12-23 13:15:05 +0800

    update: remove langfuse and enable litellm ui

--- openwebui/restart.sh
+++ openwebui/restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker compose -f docker-compose.yml -f docker-compose.searxng.yml -f docker-compose.langfuse.yml -f docker-compose.litellm.yml -p openwebui stop
-# docker compose -f docker-compose.yml -f docker-compose.searxng.yml -f docker-compose.langfuse.yml -f docker-compose.litellm.yml -p openwebui pull
-docker compose -f docker-compose.yml -f docker-compose.searxng.yml -f docker-compose.langfuse.yml -f docker-compose.litellm.yml -p openwebui up -d
+docker compose -f docker-compose.yml -f docker-compose.searxng.yml -f docker-compose.litellm.yml -p openwebui stop
+# docker compose -f docker-compose.yml -f docker-compose.searxng.yml -f docker-compose.litellm.yml -p openwebui pull
+docker compose -f docker-compose.yml -f docker-compose.searxng.yml -f docker-compose.litellm.yml -p openwebui up -d
